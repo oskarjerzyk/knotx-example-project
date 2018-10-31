618a9e20bb0a07eef7994f939db6dec033c8c3c0
Bartlomiej Malecki <bartlomiej.malecki@cognifide.com>
2018-10-31 13:55:28 +0100

    Change in start script and templates

--- acme-multistep-forms/start.sh
+++ acme-multistep-forms/start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-rm -rf ./core/verticle/*
-cp ../acme-forms-adapter-multistep/build/libs/acme-forms-adapter-multistep-1.5.0-SNAPSHOT.jar ./core/verticle/
-cp ../acme-forms-adapter-http/build/libs/acme-forms-adapter-http-1.5.0-SNAPSHOT.jar ./core/verticle/
+VERSION=1.5.0-SNAPSHOT
 
-rsync ~/.m2/repository/io/knotx/knotx-forms-*/1.5.0-SNAPSHOT/knotx-forms-*-1.5.0-SNAPSHOT.jar ./core/verticle/
+rm -rf ./core/verticle/*
+cp ../acme-forms-adapter-multistep/build/libs/acme-forms-adapter-multistep-${VERSION}.jar ./core/verticle/
+cp ../acme-forms-adapter-http/build/libs/acme-forms-adapter-http-${VERSION}.jar ./core/verticle/
+
+rsync ~/.m2/repository/io/knotx/knotx-forms-*/1.5.0-SNAPSHOT/knotx-forms-*-${VERSION}.jar ./core/verticle/
 
 docker-compose up  --force-recreate --build
