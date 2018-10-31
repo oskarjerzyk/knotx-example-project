afeaf165462638cb25823847fe663037628f3a72
Bartlomiej Malecki <bartlomiej.malecki@cognifide.com>
2018-10-31 12:58:07 +0100

    Update start script

--- acme-multistep-forms/start.sh
+++ acme-multistep-forms/start.sh
@@ -2,4 +2,8 @@
 
 rm -rf ./core/verticle/*
 cp ../acme-forms-adapter-multistep/build/libs/acme-forms-adapter-multistep-1.5.0-SNAPSHOT.jar ./core/verticle/
+cp ../acme-forms-adapter-http/build/libs/acme-forms-adapter-http-1.5.0-SNAPSHOT.jar ./core/verticle/
+
+rsync ~/.m2/repository/io/knotx/knotx-forms-*/1.5.0-SNAPSHOT/knotx-forms-*-1.5.0-SNAPSHOT.jar ./core/verticle/
+
 docker-compose up  --force-recreate --build
