6b35dd549302a0c47d7f74042ba9e5065406c610
Bartlomiej Malecki <bartlomiej.malecki@cognifide.com>
2018-11-20 11:08:33 +0100

    Working cookie session

--- acme-forms-adapter-multistep/src/main/java/com/acme/forms/adapter/multiform/MultiStepFormsSessionProxy.java
+++ acme-forms-adapter-multistep/src/main/java/com/acme/forms/adapter/multiform/MultiStepFormsSessionProxy.java
@@ -17,23 +17,28 @@ package com.acme.forms.adapter.multiform;
 
 
 import com.acme.forms.adapter.multiform.common.configuration.MultiStepFormsAdapterConfiguration;
-import io.knotx.dataobjects.ClientResponse;
+import com.google.common.net.HttpHeaders;
 import io.knotx.dataobjects.KnotContext;
 import io.knotx.knot.AbstractKnotProxy;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.cookie.Cookie;
+import io.netty.handler.codec.http.cookie.DefaultCookie;
+import io.netty.handler.codec.http.cookie.ServerCookieDecoder;
+import io.netty.handler.codec.http.cookie.ServerCookieEncoder;
 import io.reactivex.Single;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.reactivex.core.MultiMap;
 import io.vertx.reactivex.core.Vertx;
+import java.util.Random;
 import java.util.Set;
 
 
 public class MultiStepFormsSessionProxy extends AbstractKnotProxy {
 
-  private static final Logger LOGGER = LoggerFactory
-      .getLogger(MultiStepFormsSessionProxy.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(MultiStepFormsSessionProxy.class);
+
+  private static final String SESSION_NAME_COOKIE = "multiform_session";
 
   private final Vertx vertx;
 
@@ -46,14 +51,18 @@ public class MultiStepFormsSessionProxy extends AbstractKnotProxy {
 
   @Override
   public Single<KnotContext> processRequest(KnotContext knotContext) {
-    String cookie = knotContext.getClientRequest().getHeaders().get("Cookie");
+    String sessionValue = getSessionValue(knotContext);
 
-    MultiMap headers = knotContext.getClientResponse().getHeaders();
+    if (sessionValue == null) {
+      Cookie sessionCookie = new DefaultCookie(SESSION_NAME_COOKIE, generateSessionId());
 
-    headers.add("Set-Cookie", "multiform_session=157658");
-    ClientResponse clientResponse =  knotContext.getClientResponse()
-        .setHeaders(headers);
-    return Single.just(knotContext.setClientResponse(clientResponse));
+      MultiMap headers = knotContext.getClientResponse().getHeaders();
+      headers.add("Set-Cookie",
+          ServerCookieEncoder.STRICT.encode(sessionCookie));
+      knotContext.getClientResponse().setHeaders(headers);
+    }
+
+    return Single.just(knotContext);
   }
 
   @Override
@@ -68,5 +77,27 @@ public class MultiStepFormsSessionProxy extends AbstractKnotProxy {
     errorResponse.getClientResponse()
         .setStatusCode(HttpResponseStatus.INTERNAL_SERVER_ERROR.code());
     return errorResponse;
+  }
+
+
+  private String getSessionValue(KnotContext knotContext) {
+    String sessionValue = null;
+    String cookieHeader = knotContext.getClientRequest().getHeaders().get(HttpHeaders.COOKIE);
+
+    if (cookieHeader != null) {
+      Set<Cookie> cookies = ServerCookieDecoder.STRICT.decode(cookieHeader);
+
+      for (Cookie cookie : cookies) {
+        if (SESSION_NAME_COOKIE.equals(cookie.name())) {
+          sessionValue = cookie.value();
+        }
+      }
+    }
+
+    return sessionValue;
+  }
+
+  private String generateSessionId() {
+    return Integer.toString(new Random().nextInt());
   }
 }
